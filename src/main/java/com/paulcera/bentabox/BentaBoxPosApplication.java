b3cb1c5582951deb197cc7cb62d889a8bf073586
Paul Cera <pacera0925@gmail.com>
2025-01-31 21:05:03 +0800

    Initial commit

---
+++ src/main/java/com/paulcera/bentabox/BentaBoxPosApplication.java
@@ -0,0 +1,13 @@
+package com.paulcera.bentabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BentaBoxPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BentaBoxPosApplication.class, args);
+	}
+
+}
